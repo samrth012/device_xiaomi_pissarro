2857c330e38c99534dbfd91ae47a1059cd557e75
rio004 <riyon336@gmail.com>
2024-04-06 16:20:21 +0100

    pissarro: Drop vendor.lineage.trust@1.0
    Change-Id: Ia93dc7b95ffc5069afdcccd34ff34bafccd975cd
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -370,10 +370,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
