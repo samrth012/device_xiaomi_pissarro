713fb552fd7040c7c23ddfe5b93873c133a7b227
Bruno Martins <bgcngm@gmail.com>
2024-05-18 13:21:42 +0100

    pissarro: Switch to common libmtkperf_client and power-libperfmgr
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd

--- device.mk
+++ device.mk
@@ -287,7 +287,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor \
     libmtkperf_client
@@ -360,6 +360,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/mediatek/libmtkperf_client \
     hardware/mediatek \
     hardware/xiaomi
 
